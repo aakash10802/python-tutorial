e48209d40670523152110e81b27c54e0e0a1a672
Aakash <aakashpc123@gmail.com>
2025-01-07 14:23:25 +0530

    Create file_handling.py

---
+++ file_handling.py
@@ -0,0 +1,55 @@
+
+# File Handling
+
+# # Files -  to open a file
+# #Type of modes, in read, write, append and create.
+# syntaxf=open("file name","mode")  
+# file=open('sebin.txt','r')
+# # print(file.read())
+# # print(file.readlines())
+# # print(file.readline())
+# # print(file.readline())
+# file.close()
+# print(file.read())
+# # # # 
+# overwrite content in case of already created file
+ #newly add content in not existing file
+
+
+# f=open("sebin.txt","w")  
+# f.write("helloooooooo")
+
+
+# f=open("sebin1.txt","w")  
+# f.write("helloooooooo sebin")
+
+
+
+
+# f=open("minnu2.txt","w")  
+# f.write("haiiiiiiiiiiiii")
+# print(f.read())
+# f.close()
+# f.write("ammu")
+
+# f=open("abh4.txt","x")
+
+# f.write("Course of python")
+
+
+
+
+# existing file --read, write, append
+# non existing file - write, append, create
+# MODES -- READ, WRITE, APPEND, CREATE
+# r, w, a, x
+# variable=open('filename','mode')
+# variable.close()
+
+
+# file=open("minnu4.txt","x")
+# file.write("aparna")
+
+
+# existing --read, write, append
+# non existing - write, append, create
